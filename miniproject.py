feb999a4129c35990dce6ef26b7b859c29672f01
Srishti-1307 <spare@bestpeers.com>
2025-07-08 19:29:18 +0530

    Delete miniproject.py

--- miniproject.py
+++
@@ -1,8 +0,0 @@
-# Question: Parse and clean a CSV-like string input 
-
-strInput= input('enter a string: ')
-specialChar=["@","-","_","%","!","<",">","(",")","*","&","^","+","=","#","$"]
-
-for i in strInput:
-    if i not in specialChar:
-        print(i, end='')
